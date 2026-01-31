d2f3296ab703979aa034fd7cdd139821fc19d77c
brezzgg <afonin1003@gmail.com>
2026-01-31 03:40:20 +0300

    Improve clipboard logic

--- lua/cpserv/clipboard.lua
+++ lua/cpserv/clipboard.lua
@@ -2,10 +2,13 @@ local reg = require("cpserv.reg")
 
 local M = {}
 
+M.enabled = false
+
 function M.setup()
-	if M.prev == nil then
+	if M.enabled == false then
 		M.prev = vim.g.clipboard
 	end
+	M.enabled = true
 	vim.g.clipboard = {
 		name = "cpserv",
 		copy = {
@@ -20,9 +23,11 @@ function M.setup()
 end
 
 function M.undo()
-	if M.prev then
+	if M.enabled == true then
 		vim.g.clipboard = M.prev
+
 		M.prev = nil
+		M.enabled = false
 	end
 end
 
